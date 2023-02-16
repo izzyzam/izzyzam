12bad0bcb202a4ed699cf66383bc6e6695979d96
izzy <izzyzam94@gmail.com>
2023-02-16 13:39:31 -0600

    testing123

---
+++ hello.py
@@ -0,0 +1,7 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: C:/Users/gbbid_uqdxjn1/izzyzam/hello.py ===============
+hello world
+>>> print("hello everyone")
+hello everyone
