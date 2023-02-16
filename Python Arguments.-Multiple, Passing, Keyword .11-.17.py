3d2d4f83b46d73ef724112f25aab7ae2516ec429
izzy <izzyzam94@gmail.com>
2023-02-16 15:40:41 -0600

    Cisco 2.1.1.11-.17

---
+++ Python Arguments.-Multiple, Passing, Keyword .11-.17.py
@@ -0,0 +1,10 @@
+print("The itsy bitsy spider" , "climbed up", "the watersprout.")
+#2.1.1.12 this print contains 3 arguments seperated by commas.
+print("My name is", "Python.")
+print("Monty Python.")
+#2.1.1.13 Positional way of passing arguments
+#this means the first "string" will come first followed by the next "string" 
+print()
+print("My name is", "Python.", end= "Monty")
+print("Monty Python.")
+#
