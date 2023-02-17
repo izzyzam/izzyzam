f876409b27bcd5a0a0bee9f7da221a9c14c1312e
izzy <izzyzam94@gmail.com>
2023-02-17 12:59:06 -0600

    added more notes

--- Python Arguments.-Multiple, Passing, Keyword .11-.17.py
+++ Python Arguments.-Multiple, Passing, Keyword .11-.17.py
@@ -1,5 +1,5 @@
-print("The itsy bitsy spider" , "climbed up", "the watersprout.")
-#2.1.1.12 this print contains 3 arguments seperated by commas.
+print("The itsy bitsy spider" , "climbed up" , "the watersprout.")
+#2.1.1.12 this print contains 3 arguments seperated by commas. Even though they are in separate "" the output will be a full sentence
 print("My name is", "Python.")
 print("Monty Python.")
 #2.1.1.13 Positional way of passing arguments
@@ -7,4 +7,8 @@ print("Monty Python.")
 print()
 print("My name is", "Python.", end= "Monty")
 print("Monty Python.")
-#
+#2.1.1.14 Keyword arguments. These are special words change the behavior a bit.
+#Keyword argument consist of three elements: a keyword, an equal sign, and a value assigned to the argument
+#Keyword arguments have to be put after the last positional argument
+#the argument "end" is followed by "=" and the value is " 'a space marker' "
+#it seems like the end= " " took away the newline and merged the sentences together
