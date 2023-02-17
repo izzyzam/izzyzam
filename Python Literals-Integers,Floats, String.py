1476222afa84cb22dd7732d067de26fd14392464
izzy <izzyzam94@gmail.com>
2023-02-17 14:58:58 -0600

    Notes 2.2

---
+++ Python Literals-Integers,Floats, String.py
@@ -0,0 +1,17 @@
+#2.2.1.1-.12 Python Literals
+#lesson shows the different types of literals: integer, string, float, boolean values
+print(1)
+print(-3)
+#integer
+print('hi')
+#string
+print(.4)
+print(-32.09)
+print(10E2)
+print(6.62607e34)
+print(0.000001)
+#float use E or e for 'to the power of'
+print('I\'m Monty Python')
+print("I'm Monty Python")
+#use \ as an escape. use it to add parathesis inside parathesis or apostrophe.
+print('"I\m"\n""learning""\n"""Python"""')
