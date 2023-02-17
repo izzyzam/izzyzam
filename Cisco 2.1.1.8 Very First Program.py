4f4acb3d5f6a827d7239885becd58bfc9252f635
izzy <izzyzam94@gmail.com>
2023-02-17 12:44:08 -0600

    added notes

--- Cisco 2.1.1.8 Very First Program.py
+++ Cisco 2.1.1.8 Very First Program.py
@@ -1,6 +1,9 @@
 print("Hello World from Izzy so Bizzy")
 print()
+#the print() creates a line space.
 print("The itsy bitsy spider climbed up the waterspout.\n")
 print("Down came the rain and washed the spider out.")
+#the /n inside the print() adds a line space after the string
 print("\\")
+#\\ also creates a line after. If you use just a \ then it wont work. 
 print("Done")
