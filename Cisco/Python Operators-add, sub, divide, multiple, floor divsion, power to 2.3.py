b44c0de171c2e9c77aa9578ce19fd1a87148de30
izzy <izzyzam94@gmail.com>
2023-02-20 00:56:23 -0600

    Notes 2.4

---
+++ Cisco/Python Operators-add, sub, divide, multiple, floor divsion, power to 2.3.py
@@ -0,0 +1,16 @@
+print(9/36)
+print(37%9)
+print(9//2)
+print(10//2)
+print(11//2)
+print(3**2)
+print(-9.-4)
+#2.3.1.1 Python Operators
+#Python has the basic add, subtraction, multiple, and division
+#It also has % which gives you remainder of the divedents. ie:37/9 = 4 with a remainder of one. One is what % will print.
+#// gives you the floor division, meaning it rounds down the number it divides to the closest.
+#** is "to the power of"
+#you can use integers and floats
+print(2*3%5)
+#python has a list of operator priorities it goes **, +, -(positive or negative), *, /, //, %, +, -(plus or minus)
+print((2**4), (2*4.), (2*4))
