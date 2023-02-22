6c29cb1fe2383b9393567bbeafbc226174e01001
izzy <izzyzam94@gmail.com>
2023-02-22 11:38:29 -0600

    commit

---
+++ Cisco/Hello input.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+print("Hello, " + name + ". Nice to meet you!\n")
+print(name + " are you smarter than a 5th grader?")
+response_1 = input()
+if response_1 == "yes\n":
+    print("Well..." + name + " probably not!")
+else:
+    response_1 == "no\n"
+    print("Well..." + name + "at least you know!")
+print("Just joking around! " + name)
---
+++ Created a Rectangle.py
@@ -0,0 +1,4 @@
+print("+" + 10 * "-" + "+")
+print(("|" + " " * 10 + "|\n") * 5, end="")
+print("+" + 10 * "-" + "+")
+#drawing a simple rectangle! see how you're using the number * string to create the top and bottom while also creating the space inside.
